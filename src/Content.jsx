d9c76f2c842a03d3d57e2d53296ef3198424e0ec
Stas Prohetamine <prohetamine@gmail.com>
2025-05-30 05:17:26 +0300

    update

--- src/Content.jsx
+++ src/Content.jsx
@@ -139,6 +139,89 @@ const Content = () => {
     }
   }, [lastTransactionLt, isFirstLoadAction, address, images, _type, _id])
 
+  useEffect(() => {
+    const IntervalId = setInterval(async () => {
+      setFirstLoadAction(true)
+      
+      try {
+        const data = await fetch(`https://toncenter.com/api/v3/actions?account=${address}&limit=${1000}&sort=desc&supported_action_types[]=multisig_create_order&supported_action_types[]=multisig_approve&supported_action_types[]=multisig_execute&supported_action_types[]=jvault_stake&supported_action_types[]=jvault_claim&supported_action_types[]=vesting_send_message&supported_action_types[]=vesting_add_whitelist&supported_action_types[]=evaa_supply&supported_action_types[]=evaa_withdraw`).then(data => data.json())
+        if (data?.actions?.length) {
+          setLastTransactionLt(data.actions[data?.actions?.length - 1]?.end_lt)
+        }
+        
+        data.actions
+          ?.map(action => action?.details?.comment?.split('@'))
+          ?.filter(data => data)
+          ?.map(([app, id, title, description, type, nsfw, chunkAmount, position, data, isOk]) => {
+            try {
+              if (
+                chunkAmount - 0 === parseFloat(parseFloat(chunkAmount).toFixed(2)) &&
+                isOk === 'ok' &&
+                app === 'tonbingo3' && 
+                id.length === 4 &&
+                id === _id &&
+                title.length > 0 && 
+                title.length < 51 && 
+                description.length > 0 && 
+                description.length < 200 && 
+                (type === _type) && 
+                (nsfw === 'y' || nsfw === 'n') && 
+                position.match(/\d+:\d+x\d+:\d+/) && 
+                data.match(/data:/)
+              ) {
+                if (!window.data) {
+                  window.data = {}
+                }
+
+                if (!window.data[address]) {
+                  window.data[address] = {}
+                }
+
+                const [_x, _y, _mx, _my] = position.split('x').map(c => c.split(':')).flat()
+
+                const x = parseInt(_x)
+                    , y = parseInt(_y)
+                    , mx = parseInt(_mx)
+                    , my = parseInt(_my)
+
+                if (!window.data[address][id]) {
+                  window.data[address][id] = {
+                    title,
+                    type, 
+                    nsfw,
+                    id,
+                    chunkAmount,
+                    description,
+                    max: { 
+                      x: mx,
+                      y: my
+                    },
+                    chunks: {}
+                  }
+                } 
+                
+                window.data[address][id].chunks[position] = {
+                  x, 
+                  y,
+                  data
+                }
+
+                const _images = Object.keys(window.data[address]).map(id => window.data[address][id])
+
+                if (images.length !== _images.length) {
+                  setImages(_images)
+                }
+              }
+            } catch (e) {}
+          })
+      } catch (e) {}
+    }, isFirstLoadAction ? 10000 : 1000)
+
+    return () => {
+      clearInterval(IntervalId)
+    }
+  }, [lastTransactionLt, isFirstLoadAction, address, images, _type, _id])
+
   const image = images.filter(image => `${image.id}` === _id)[0]
 
   return (
